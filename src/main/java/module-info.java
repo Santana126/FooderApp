483a73552dd5ec2f00639c5deeaac7ab551b65ca
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-03-24 12:14:52 +0100

    Test -1- Github

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.santana.fooderapp {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.santana.fooderapp to javafx.fxml;
     exports com.santana.fooderapp;
 }
